3d5cd7e98299459b04d25ca6ac60e42d56304491
Khris LOICHET <khloichet@IUTCLINFA2319.iut.local>
2021-11-26 17:31:39 +0100

    Debut de la page menu et de son css

--- source/src/Main.java
+++ source/src/Main.java
@@ -1,17 +1,17 @@
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import modele.Collision;
-import modele.Personnage;
+import javafx.scene.layout.AnchorPane;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import java.io.IOException;
 
 public class Main extends Application{
     @Override
-    public void start(Stage stage){
-
+    public void start(Stage stage) throws IOException {
+        AnchorPane a = FXMLLoader.load(getClass().getResource("/menu.fxml"));
+        Scene s = new Scene(a, 500, 500);
+        s.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        stage.setScene(s);
         stage.show();
     }
 }
